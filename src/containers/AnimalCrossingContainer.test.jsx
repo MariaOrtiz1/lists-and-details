2cf7010d2ba4dbb30988d1dcd36d4b81c4aa3b72
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-09-16 10:47:48 -0700

    fixxed mock server test

--- src/containers/AnimalCrossingContainer.test.jsx
+++ src/containers/AnimalCrossingContainer.test.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import AnimalCrossingContainer from './AnimalCrossingContainer';
-// import { rest } from 'msw';
-// import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import { MemoryRouter } from 'react-router-dom';
 
-// const mockServer = setupServer(
-//   rest.get('https://ac-vill.herokuapp.com/villagers', (req, res, ctx) => {
-//     return res(
-//       ctx.json({
-//         results: [
-//           {
-//             id: '5f5fb4bbbfd05c2aed8e474',
-//             name: 'Aurora',
-//             image: 'example.com/image.png',
-//             species: 'penguin',
-//             personality: 'normal',
-//             quote: 'Always keep your cool.',
-//           }
-//         ]
-//       })
-//     );
-//   })
-// );
+const mockServer = setupServer(
+  rest.get('https://ac-vill.herokuapp.com/villagers', (req, res, ctx) => {
+    return res(
+      ctx.json(
+        [
+          {
+            id: '5f5fb4bbbfd05c2aed8e474',
+            name: 'Aurora',
+            image: 'example.com/image.png',
+            species: 'penguin',
+            personality: 'normal',
+            quote: 'Always keep your cool.',
+          }
+        ]
+      )
+    );
+  })
+);
 
 describe('AnimalCrossingContainer', () => {
-//   beforeAll(() => mockServer.listen());
-//   afterAll(() => mockServer.close());
+  beforeAll(() => mockServer.listen());
+  afterAll(() => mockServer.close());
   
   it('renders a list of villagers to the page', async () => {
     render(<MemoryRouter><AnimalCrossingContainer /></MemoryRouter>);
